b82d0495e5b552411c8b9a0ade860106d74fec6d
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-04-16 00:26:45 -0400

    ARCHIVOS INICIALES

---
+++ src/main/java/com/exp2_s6_briam_carrasco/gestionfacturasbd/GestionfacturasbdApplication.java
@@ -0,0 +1,13 @@
+package com.exp2_s6_briam_carrasco.gestionfacturasbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfacturasbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionfacturasbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp2_s6_briam_carrasco/gestionfacturasbd/GestionfacturasbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp2_s6_briam_carrasco.gestionfacturasbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionfacturasbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
