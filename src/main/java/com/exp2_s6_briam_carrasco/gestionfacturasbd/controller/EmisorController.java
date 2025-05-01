022d7441a8bd2ec5ab37c8769eaed518f820ae1d
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-05-01 02:41:26 -0400

    Se integra librería hateoas a controller y entidad emisor

--- src/main/java/com/exp2_s6_briam_carrasco/gestionfacturasbd/controller/EmisorController.java
+++ src/main/java/com/exp2_s6_briam_carrasco/gestionfacturasbd/controller/EmisorController.java
@@ -2,8 +2,12 @@ package com.exp2_s6_briam_carrasco.gestionfacturasbd.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exp2_s6_briam_carrasco.gestionfacturasbd.model.Cliente;
 import com.exp2_s6_briam_carrasco.gestionfacturasbd.model.Emisor;
 import com.exp2_s6_briam_carrasco.gestionfacturasbd.service.EmisorService;
 
@@ -25,29 +30,55 @@ public class EmisorController {
     @Autowired
     private EmisorService emisorService;
 
+
     @GetMapping
-    public List<Emisor> getAllEmisores() {
-        return emisorService.getAllEmisores();
+    public CollectionModel<EntityModel<Emisor>> getAllEmisores() {
+        List<Emisor> emisores = emisorService.getAllEmisores();
+
+        List<EntityModel<Emisor>> emisorResources = emisores.stream()
+                .map(emisor -> EntityModel.of(emisor,
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getEmisorById(emisor.getIdEmisor())).withSelfRel()))
+                .collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getAllEmisores());
+        CollectionModel<EntityModel<Emisor>> resources = CollectionModel.of(emisorResources, linkTo.withRel("emisores"));
+
+        return resources;
     }
 
     @GetMapping("/{id}")
-    public Optional<Emisor> getEmisorById(@PathVariable Long id) {
-        return emisorService.getEmisorById(id);
+    public EntityModel<Emisor> getEmisorById(@PathVariable Long id) {
+        Optional<Emisor> emisor = emisorService.getEmisorById(id);
+        if (emisor.isPresent()) {
+            return EntityModel.of(emisor.get(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getEmisorById(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getAllEmisores()).withRel("all-emisores"));
+        } else {
+            throw new RuntimeException("Emisor no encontrado con id: " + id);
+        }
     }
 
     @PostMapping
-    public Emisor createEmisor(@RequestBody Emisor emisor) {
-        return emisorService.createEmisor(emisor);
+    public EntityModel<Emisor> createEmisor(@RequestBody Emisor emisor) {
+        Emisor createdEmisor = emisorService.createEmisor(emisor);
+        return EntityModel.of(createdEmisor,
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getEmisorById(createdEmisor.getIdEmisor())).withSelfRel(),
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getAllEmisores()).withRel("all-emisores"));
     }
 
     @PutMapping("/{id}")
-    public Emisor updateEmisor(@PathVariable Long id, @RequestBody Emisor emisor) {
-        return emisorService.updateEmisor(id, emisor);
+    public EntityModel<Emisor> updateEmisor(@PathVariable Long id, @RequestBody Emisor emisor) {
+        Emisor updatedEmisor = emisorService.updateEmisor(id, emisor);
+        return EntityModel.of(updatedEmisor,
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getEmisorById(updatedEmisor.getIdEmisor())).withSelfRel(),
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmisorController.class).getAllEmisores()).withRel("all-emisores"));
     }
 
     @DeleteMapping("/{id}")
     public void deleteEmisor(@PathVariable Long id) {
         emisorService.deleteEmisor(id);
     }
-    
 }
+
+    
+
